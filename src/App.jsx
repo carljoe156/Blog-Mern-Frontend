1a3527034f546f3a640db72a931812799dfc75cc
Carl Joseph <carljoseph156@gmail.com>
2025-02-14 16:57:14 -0500

    Added/Updated CreatePost and main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import LoginSignup from "./components/LoginSignup";
 import DataProvider from "./components/context/dataProvider.jsx";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
-
+import CreatePost from "./pages/CreatePost";
 function App() {
   return (
     <DataProvider>
       <Routes>
         <Route path="/" element={<LoginSignup />}></Route>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/create" element={<CreatePost />} />
       </Routes>
     </DataProvider>
   );
